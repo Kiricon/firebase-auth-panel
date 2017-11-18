0772719738a1bbfe7854e69c97eb65c82e32924e
Dominic Valenciana <kiricon@live.com>
2017-11-18 14:58:36 -0700

    submit on enter is now in place

--- firebase-auth-panel.js
+++ firebase-auth-panel.js
@@ -230,6 +230,7 @@ template.innerHTML = `
     </div>
     <div class="content">
         <img src="" />
+        
         <div class="control">
             <svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
                 <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/>
@@ -315,6 +316,12 @@ class FirebaseLogin extends HTMLElement {
 
         this.loginButton.addEventListener('click', () => {
             this.loginUser(this.emailInput.value, this.passwordInput.value);
+        });
+
+        this.passwordInput.addEventListener('keyup', (e) => {
+            if(e.keyCode == 13) {
+                this.loginUser(this.emailInput.value, this.passwordInput.value);
+            }
         });
 
         this.listenForUser();
